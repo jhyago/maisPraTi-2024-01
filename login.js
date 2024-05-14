d96636c9c33717c5ccf34f82c94f2871234250bf
jhyago <jaquesoantunes@gmail.com>
2024-05-13 21:54:01 -0300

    Vitti mandou

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 function checkPassword(password){
     if(password === '2341'){
         console.log("senha incorreta")
+    } else {
+        console.log('senha correta')
     }
 }
 function validateWebService(){}
