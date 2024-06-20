18115195f6d87e7cbc15cad7b41c36deb5ca803c
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-06-19 23:36:16 -0300

    fix: Carousel slide change function

--- unit03/lesson14/projetoModulo/assets/js/app.js
+++ unit03/lesson14/projetoModulo/assets/js/app.js
@@ -23,60 +23,16 @@ checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
 
+const images = document.querySelectorAll(".corousel-images img");
 
-const images = document.querySelectorAll('.corousel-images img')
+let slideIndex = 0;
 
-let slideIndex = 1
-
-function next() {
-
-    for(let i = 0; i < images.length; i++) {
-        images[i].src = './assets/images/img' + Math.abs((slideIndex + i) % images.length) + '.png'
-    }
-
-    // space1control++
-    // space2control++
-    // space3control++
-
-    // if(space1control > 3){
-    //     space1control = 1
-    // }
-
-    // if(space2control > 3){
-    //     space2control = 1
-    // }
-
-    // if(space3control > 3){
-    //     space3control = 1
-    // }
-
-    // space1.src = './assets/images/img' + space1control + '.png'
-    // space2.src = './assets/images/img' + space2control + '.png'
-    // space3.src = './assets/images/img' + space3control + '.png'
-}
-
-function back() {
-    let space1 = document.getElementById('space1')
-    let space2 = document.getElementById('space2')
-    let space3 = document.getElementById('space3')
-
-    space1control--
-    space2control--
-    space3control--
-
-    if(space1control < 1){
-        space1control = 3
-    }
-
-    if(space2control < 1){
-        space2control = 3
-    }
-
-    if(space3control < 1){
-        space3control = 3
-    }
-
-    space1.src = './assets/images/img' + space1control + '.png'
-    space2.src = './assets/images/img' + space2control + '.png'
-    space3.src = './assets/images/img' + space3control + '.png'
-}
+function handleSlideChange(value) {
+  slideIndex += value;
+  for (let i = 0; i < images.length; i++) {
+    images[i].src =
+      "./assets/images/img" +
+      ((Math.abs(slideIndex + i) % images.length) + 1) +
+      ".png";
+  }
+};
