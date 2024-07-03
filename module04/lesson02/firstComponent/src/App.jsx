0aa5fd732ea7064ecc3d63060a2d6e31e2adee27
jhyago <jaquesoantunes@gmail.com>
2024-07-03 20:21:21 -0300

    First Component

---
+++ module04/lesson02/firstComponent/src/App.jsx
@@ -0,0 +1,7 @@
+import HelloWorld from "./components/HelloWorld"
+
+function App() {
+  return <HelloWorld/>
+}
+
+export default App
---
+++ module04/lesson02/firstComponent/src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+function HelloWorld(){
+    return (
+        <div>
+            <h1>Hello world, sou o Edu</h1>
+            <p>Ou o Jaques, depende.</p>
+        </div>
+    )
+}
+
+export default HelloWorld
