df67226d7b3f243e412c8115f0af115e40bc1c43
jhyago <jaquesoantunes@gmail.com>
2024-07-02 21:08:00 -0300

    Jeferson pediu

--- unit04/lesson01/app.js
+++ unit04/lesson01/app.js
@@ -22,14 +22,20 @@ const btnTab3 = document.getElementById('btn-tab3')
 const content = document.getElementById('content')
 
 function displayContent(items) {
-
+    let listaConteudo = ""
+    for (const item of items){
+        listaConteudo += `<li>${item}</li>`
+    }
+    const lista = document.createElement('ul')
+    content.innerHTML = listaConteudo
+    content.append(lista)
 }
 
 function activateButton(btn) {
-    btnTab1.classname = ""
-    btnTab2.classname = ""
-    btnTab3.classname = ""
-    btn.classname = 'active'
+    btnTab1.className = ""
+    btnTab2.className = ""
+    btnTab3.className = ""
+    btn.className = "active"
 }
 
 function handleClick(event) {
@@ -37,9 +43,15 @@ function handleClick(event) {
     activateButton(event.target)
 
     if(btnId === "btn-tab1"){
-        displayContent(content[0])
+        displayContent(conteudos[0])
+    } else if (btnId === "btn-tab2") {
+        displayContent(conteudos[1])
+    } else {
+        displayContent(conteudos[2])
     }
 }
+
+displayContent(conteudos[0])
 
 btnTab1.addEventListener("click", handleClick)
 btnTab2.addEventListener("click", handleClick)
