781f1a699c451150064b043d2222916f039cf780
jhyago <jaquesoantunes@gmail.com>
2024-07-04 09:58:00 -0300

    adding comments

--- module04/lesson02/props/src/App.jsx
+++ module04/lesson02/props/src/App.jsx
@@ -1,16 +1,21 @@
-import Props from "./components/props"
+// Importa o componente BookDetails do arquivo ./components/BookDetails
+import BookDetails from "./components/BookDetails"
 
+// Define um objeto com os detalhes do autor e seu livro favorito
 const item = {
     author: "Jaques",
     favoriteBook: "Teste"
 }
 
+// Define o componente App que utiliza o componente BookDetails
 function App() {
     return (
         <div>
-            <Props props={item}/>
+            {/* Passa o objeto item desestruturado como props para o componente BookDetails */}
+            <BookDetails author={item.author} favoriteBook={item.favoriteBook} />
         </div>
     )
 }
 
+// Exporta o componente App para ser usado em outros arquivos
 export default App
