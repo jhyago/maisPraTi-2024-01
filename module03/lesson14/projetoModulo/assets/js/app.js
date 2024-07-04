8a609641ac6bd16bc045785d84ed69a5ae63f2d1
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-07-04 10:31:40 -0300

    fix: carousel module03/lesson14

--- module03/lesson14/projetoModulo/assets/js/app.js
+++ module03/lesson14/projetoModulo/assets/js/app.js
@@ -1,83 +1,38 @@
-const hamburger = document.querySelector("#hamburger")
-const navMenu = document.querySelector("ul")
-const navLink = document.querySelectorAll("#nav-link")
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("ul");
+const navLink = document.querySelectorAll("#nav-link");
 
-const scrollUp = document.querySelector("#scroll-up")
+const scrollUp = document.querySelector("#scroll-up");
 
-hamburger.addEventListener("click", openMenu)
+hamburger.addEventListener("click", openMenu);
 
-const checkbox = document.querySelector("#checkbox")
+const checkbox = document.querySelector("#checkbox");
 
 function openMenu() {
-  hamburger.classList.toggle("active")
-  navMenu.classList.toggle("active")
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu))
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 function closeMenu() {
-  hamburger.classList.remove("active")
-  navMenu.classList.remove("active")
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
-})
+  document.body.classList.toggle("dark");
+});
 
-const images = document.querySelectorAll(".corousel-images img")
+const images = document.querySelectorAll(".corousel-images img");
 
-let slideIndex = 0
+let slideIndex = 0;
 
-function next() {
+function handleSlideChange(value) {
+  slideIndex += value;
   for (let i = 0; i < images.length; i++) {
     images[i].src =
       "./assets/images/img" +
-      Math.abs((slideIndex + i) % images.length) +
-      ".png"
+      ((Math.abs(slideIndex + i) % images.length) + 1) +
+      ".png";
   }
-
-  // space1control++
-  // space2control++
-  // space3control++
-
-  // if(space1control > 3){
-  //     space1control = 1
-  // }
-
-  // if(space2control > 3){
-  //     space2control = 1
-  // }
-
-  // if(space3control > 3){
-  //     space3control = 1
-  // }
-
-  // space1.src = './assets/images/img' + space1control + '.png'
-  // space2.src = './assets/images/img' + space2control + '.png'
-  // space3.src = './assets/images/img' + space3control + '.png'
-}
-
-function back() {
-  let space1 = document.getElementById("space1")
-  let space2 = document.getElementById("space2")
-  let space3 = document.getElementById("space3")
-
-  space1control--
-  space2control--
-  space3control--
-
-  if (space1control < 1) {
-    space1control = 3
-  }
-
-  if (space2control < 1) {
-    space2control = 3
-  }
-
-  if (space3control < 1) {
-    space3control = 3
-  }
-
-  space1.src = "./assets/images/img" + space1control + ".png"
-  space2.src = "./assets/images/img" + space2control + ".png"
-  space3.src = "./assets/images/img" + space3control + ".png"
-}
+};
