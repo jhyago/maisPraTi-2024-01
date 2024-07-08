e64372fa1b7d2fbba91095201c26081b74b1769f
jhyago <jaquesoantunes@gmail.com>
2024-07-08 20:18:47 -0300

    navegacao-abas

---
+++ module04/lesson04/navegacao-abas/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavegacaoAbas from './componentes/NavegacaoAbas'
+
+function App() {
+
+  return (
+    <>
+      <NavegacaoAbas />
+    </>
+  )
+}
+
+export default App
