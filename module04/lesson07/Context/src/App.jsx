f5d067374818a3528c6cf6666f2f054075eb281c
jhyago <jaquesoantunes@gmail.com>
2024-07-15 20:33:59 -0300

    themeContext

--- module04/lesson07/Context/src/App.jsx
+++ module04/lesson07/Context/src/App.jsx
@@ -1,14 +1,37 @@
-import MyChildren from "./components/Filho"
-import { MyProvider } from "./components/Contexto"
+// import MyChildren from "./components/Filho"
+// import { MyProvider } from "./components/Contexto"
+import { useState, createContext, useContext } from 'react'
+
+const ThemeContext = createContext({ theme: 'light', toggleTheme: () =>{} })
 
 function App() {
 
+  const [ theme, setTheme ] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light')
+  }
+
   return (
     <>
-      <MyProvider>
-        <MyChildren/>
-      </MyProvider>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <div>
+            <Toolbar />
+            <button onClick={toggleTheme}>Trocar o tema</button>
+          </div>
+      </ThemeContext.Provider>
+
     </>
+  )
+}
+
+function Toolbar() {
+  const { theme } = useContext(ThemeContext)
+
+  return (
+    <div style={{background: theme === 'dark' ? 'grey' : 'blue', color: theme === 'dark' ? 'white' : 'white'}}>
+      Tema utilizado - {theme}
+    </div>
   )
 }
 
--- module04/lesson07/Context/src/components/Contexto.jsx
+++ module04/lesson07/Context/src/components/Contexto.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { createContext } from "react"
 
 export const MyContext = createContext()
