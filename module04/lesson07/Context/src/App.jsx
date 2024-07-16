f9c0d0045955c408bed5774475cefcc28cd0d42b
jhyago <jaquesoantunes@gmail.com>
2024-07-15 22:08:52 -0300

    changePreferences

--- module04/lesson07/Context/src/App.jsx
+++ module04/lesson07/Context/src/App.jsx
@@ -1,36 +1,48 @@
+//DESAFIO 1
+
 // import MyChildren from "./components/Filho"
 // import { MyProvider } from "./components/Contexto"
 import { useState, createContext, useContext } from 'react'
 
-const ThemeContext = createContext({ theme: 'light', toggleTheme: () =>{} })
+const PreferencesContext = createContext()
 
 function App() {
 
-  const [ theme, setTheme ] = useState('light')
+  const [ preferences, setPreferences ] = useState({ theme: 'light', language: 'en'})
 
   const toggleTheme = () => {
-    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light')
+    setPreferences(currentTheme => ({
+      theme: currentTheme.theme === 'light' ? 'dark' : 'light'}))
+  }
+
+  const changeLanguage = (language) => {
+    setPreferences(currentLanguage => ({
+      ...currentLanguage,
+      language: language
+    }))
   }
 
   return (
     <>
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <PreferencesContext.Provider value={{ preferences, toggleTheme, changeLanguage }}>
           <div>
             <Toolbar />
             <button onClick={toggleTheme}>Trocar o tema</button>
+            <button onClick={() => changeLanguage('pt-br')}>PT-BR</button>
+            <button onClick={() => changeLanguage('en')}>EN</button>
           </div>
-      </ThemeContext.Provider>
+      </PreferencesContext.Provider>
 
     </>
   )
 }
 
 function Toolbar() {
-  const { theme } = useContext(ThemeContext)
+  const { preferences } = useContext(PreferencesContext)
 
   return (
-    <div style={{background: theme === 'dark' ? 'grey' : 'blue', color: theme === 'dark' ? 'white' : 'white'}}>
-      Tema utilizado - {theme}
+    <div style={{background: preferences.theme === 'dark' ? 'grey' : 'blue', color: preferences.theme === 'dark' ? 'white' : 'white'}}>
+      Tema utilizado - {preferences.theme}, Idioma: {preferences.language}
     </div>
   )
 }
