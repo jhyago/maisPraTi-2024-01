a7a54617850afc0a3169d0ae6ad30dabda3bcb9a
jhyago <jaquesoantunes@gmail.com>
2024-07-22 20:23:27 -0300

    fix bug

---
+++ module04/lesson10/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LanguageTranslator from './components/LanguageTranslator'
+
+function App() {
+
+  return (
+    <>
+      <LanguageTranslator/>
+    </>
+  )
+}
+
+export default App
