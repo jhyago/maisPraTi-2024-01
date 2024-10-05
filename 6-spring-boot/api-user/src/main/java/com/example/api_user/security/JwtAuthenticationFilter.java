5198ad569ef1b6bfdd2d35bab05830f1d7ecae19
jhyago <jaquesoantunes@gmail.com>
2024-10-05 12:04:50 -0300

    lesson73

---
+++ 6-spring-boot/api-user/src/main/java/com/example/api_user/security/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.example.api_user.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, CustomUserDetailsService userDetailsService){
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+            String authHeader = request.getHeader("Authorization");
+
+            if(authHeader == null || !authHeader.startsWith("Bearer" )){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String jwt = authHeader.substring(7);
+
+            String username = jwtTokenProvider.extractUsername(jwt);
+
+            if(username != null &&
+                    SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            }
+
+            if(jwtTokenProvider.isTokenValid((jwt, userDetails))){
+
+        }
+    }
+}
