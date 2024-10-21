44650827e6346f7858c1ff0f57790f4665afc9a0
jhyago <jaquesoantunes@gmail.com>
2024-10-21 20:25:27 -0300

    lesson79

---
+++ 6-spring-boot/JWT-OAuth2Demo/src/main/java/com/example/JWT_OAuth2Demo/securityOAuthConfig/TokenGenerator.java
@@ -0,0 +1,88 @@
+package com.example.JWT_OAuth2Demo.securityOAuthConfig;
+
+import com.example.JWT_OAuth2Demo.userDocument.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Component
+public class TokenGenerator {
+    @Autowired
+    JwtEncoder accessTokenEncoder;
+
+    @Autowired
+    @Qualifier("refreshTokenEncoder")
+    JwtEncoder refreshTokenEncoder;
+
+    private String createAccessToken(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Instant now = Instant.now();
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("Jaques")
+                .issuedAt(now)
+                .expiresAt(now.plus(5, ChronoUnit.MINUTES))
+                .subject(user.getId())
+                .build();
+
+        return accessTokenEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+
+    private String createRefreshToken(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Instant now = Instant.now();
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("Jaques")
+                .issuedAt(now)
+                .expiresAt(now.plus(30, ChronoUnit.DAYS))
+                .subject(user.getId())
+                .build();
+
+        return refreshTokenEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+
+    public Token (Authentication authentication) {
+        if(!(authentication.getPrincipal() instanceof User user)){
+            throw new BadCredentialsException(new MessageFormat("principal {0} não é do tipo User",
+                    authentication.getPrincipal().getClass())
+            );
+        }
+
+        Token tokenDTO = new Token();
+        tokenDTO.setUserId(user.getId());
+        tokenDTO.setAccessToken(createAccessToken(authentication));
+
+        String refreshToken;
+
+        if(authentication.getCredentials() instanceof Jwt jwt){
+            Instant now = Instant.now();
+            Instant expiresAt = jwt.getExpiresAt();
+            Duration duration = Duration.between(now, expiresAt);
+            long daysUntilExpired = duration.toDays();
+
+            if(daysUntilExpired < 7){
+                refreshToken = createRefreshToken(authentication);
+            } else {
+                refreshToken = jwt.getTokenValue();
+            }
+        } else {
+            refreshToken = createRefreshToken(authentication);
+        }
+
+        tokenDTO.setRefreshToken(refreshToken);
+
+        return tokenDTO;
+    }
+}
--- 6-spring-boot/JWT-OAuth2Demo/src/main/java/com/example/JWT_OAuth2Demo/securityOAuthConfig/WebSecurity.java
+++ 6-spring-boot/JWT-OAuth2Demo/src/main/java/com/example/JWT_OAuth2Demo/securityOAuthConfig/WebSecurity.java
@@ -1,14 +1,28 @@
 package com.example.JWT_OAuth2Demo.securityOAuthConfig;
 
-import com.nimbusds.jose.util.KeyUtils;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.JWTProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -43,5 +57,49 @@ public class WebSecurity {
                 .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
 
         return http.build();
+    }
+
+    @Bean
+    @Primary
+    JwtDecoder jwtAccessTokenDecoder() {
+        JWK jwk = new RSAKey.Builder(keyUtils.getAccessTokenPublicKey()).privateKey(keyUtils.getAccessTokenPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtDecoder((JWTProcessor<SecurityContext>) jwks);
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenEncoder")
+    JwtEncoder jwtRefreshTokenEncoder(){
+        JWK jwk = new RSAKey.Builder(keyUtils.getRefreshTokenPublicKey()).privateKey(keyUtils.getRefreshTokenPrivateKey()).build();
+
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenDecoder")
+    JwtDecoder jwtRefreshTokenDecoder(){
+
+        return NimbusJwtDecoder.withPublicKey(keyUtils.getRefreshTokenPublicKey()).build();
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenAuthProvider")
+    JwtAuthenticationProvider jwtRefreshTokenAuthProvider() {
+        JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtRefreshTokenDecoder());
+        provider.setJwtAuthenticationConverter(jwtToUserConverter);
+        return provider;
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsManager);
+
+        return provider;
     }
 }
